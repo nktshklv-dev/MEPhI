ad3820010b3fe33e0089a2ed0a328b0a2bd33e1c
Nikita Shakalov <nikitashakalov@gmail.com>
2023-09-11 16:00:19 +0300

    add comments

--- laba1/part2/lab1p2.c
+++ laba1/part2/lab1p2.c
@@ -6,25 +6,11 @@ int main(void){
     int n2;
     int n3; 
     scanf("%d%d%d", &n1, &n2, &n3); 
+    //Проверка на одинаковые числа
     if (n1 == n2 && n2 == n3){ 
         printf("%d=%d=%d\n", n1, n2, n3); 
     } 
-    else if(n1 == n2 && n2 != n3){ 
-        if(n2 > n3){ 
-            printf("%d=%d>%d\n", n1, n2, n3); 
-        } 
-        else{ 
-            printf("%d=%d<%d\n", n1, n2, n3); 
-        } 
-    } 
-    else if(n1 == n3 && n3 != n2){ 
-        if (n3 > n2){ 
-            printf("%d=%d>%d\n", n1, n3, n2); 
-        } 
-        else{ 
-            printf("%d=%d<%d\n", n1, n3, n2); 
-        } 
-    } 
+    //Проверка на наименьшее первое число 
     else if(n2 == n3 && n3 != n1){ 
         if (n3 > n1){ 
             printf("%d=%d>%d\n", n2, n3, n1); 
@@ -33,6 +19,25 @@ int main(void){
             printf("%d=%d<%d\n", n2, n3, n1); 
         } 
     } 
+    //Проверка на наименьшее второе число 
+     else if(n1 == n3 && n3 != n2){ 
+        if (n3 > n2){ 
+            printf("%d=%d>%d\n", n1, n3, n2); 
+        } 
+        else{ 
+            printf("%d=%d<%d\n", n1, n3, n2); 
+        } 
+    } 
+    //Проверка на наименьшее третье число 
+    else if(n1 == n2 && n2 != n3){ 
+        if(n2 > n3){ 
+            printf("%d=%d>%d\n", n1, n2, n3); 
+        } 
+        else{ 
+            printf("%d=%d<%d\n", n1, n2, n3); 
+        } 
+    } 
+    //Поиск среднего числа 
     else if((n1 > n2 && n1 < n3) || (n1 < n2 && n1 > n3)){ 
         printf("%d\n", n1); 
     } 
