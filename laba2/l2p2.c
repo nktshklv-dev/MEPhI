aeb478d014c181083f57d6fa863854a475f07c6f
Nikita Shakalov <nikitashakalov@gmail.com>
2023-09-18 14:56:37 +0300

    laba2p1

--- laba2/l2p2.c
+++ laba2/l2p2.c
@@ -5,12 +5,12 @@ int main(void)
    int m; 
    int k = 0; 
    int a = 7;
+
    scanf("%d", &m);
-    while (a <= m) {
+    while (a < m) {
         a *= 7;
         k++;
     }
-
     printf("%d\n", k);
    
     return 0;
